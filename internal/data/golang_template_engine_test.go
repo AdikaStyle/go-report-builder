d416ae5afe6cb52e7236d4e5fca3782232d46d50
matang <matang28@gmail.com>
2021-01-20 12:38:55 +0200

    Adding Golang template engine

---
+++ internal/data/golang_template_engine_test.go
@@ -0,0 +1,37 @@
+package data
+
+import (
+	"github.com/AdikaStyle/go-report-builder/internal/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var golangTemplateRepo = NewGolangTemplateEngine()
+
+func TestGolangTemplateRepository_Render(t *testing.T) {
+	assertTemplate(t,
+		"{{ .Values.name }}",
+		map[string]interface{}{"name": "matan"},
+		"matan",
+	)
+
+	assertTemplate(t,
+		"hello {{ .Values.name }}",
+		map[string]interface{}{"name": "matan"},
+		"hello matan",
+	)
+
+	assertTemplate(t, `{{- range .Values.items }}
+{{ . }}
+{{- end }}
+`,
+		map[string]interface{}{"items": []int{1, 2, 3, 4}},
+		"\n1\n2\n3\n4\n",
+	)
+}
+
+func assertTemplate(t *testing.T, template string, data interface{}, result string) {
+	rendered, err := golangTemplateRepo.Render([]byte(template), &models.TemplateContext{Values: data})
+	assert.Nil(t, err)
+	assert.EqualValues(t, result, rendered)
+}

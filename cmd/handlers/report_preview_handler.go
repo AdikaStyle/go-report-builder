e07184210178d532effb5b7a0dae561d434e948c
matang <matang28@gmail.com>
2021-01-22 18:00:07 +0200

    Adding the preview handler to the application layer

--- cmd/handlers/report_preview_handler.go
+++ cmd/handlers/report_preview_handler.go
@@ -9,10 +9,19 @@ import (
 	"net/http"
 )
 
-func ReportPreviewHandler(templateEngine data.TemplateEngine, reportService business.ReportService) gin.HandlerFunc {
+func ReportPreviewHandler(templateService business.TemplateService,
+	templateEngine data.TemplateEngine,
+	reportService business.ReportService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		reportId := ctx.Param("reportId")
 		data, err := extractData(ctx)
+		if err != nil {
+			logrus.Error(err)
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		templates, err := templateService.ListTemplates()
 		if err != nil {
 			logrus.Error(err)
 			ctx.String(http.StatusInternalServerError, err.Error())
@@ -30,6 +39,7 @@ func ReportPreviewHandler(templateEngine data.TemplateEngine, reportService busi
 			[]byte(previewTemplate),
 			&models.TemplateContext{Values: map[string]interface{}{
 				"reportId": reportId,
+				"reports":  templates,
 				"data":     data,
 				"image":    string(png),
 			}},
@@ -48,6 +58,9 @@ const previewTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.1.8/jsoneditor.min.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.1.8/jsoneditor.min.js"></script>
 	<style>
 		
 	</style>
@@ -55,10 +68,55 @@ const previewTemplate = `
 <body>
 
 <div>
-	Preview App:
-	<p> {{ .Values.reportId }} </p>
-	<p> {{ .Values.data }} </p>
-	<img src="data:image/png;base64, {{ .Values.image }}" alt="Report" />
+	<h1>go-report-builder</h1>
+	<h2>Preview Tool</h2>
+	
+	<p>Selected Report:</p>
+	<select id="reports">
+		{{- range $c := .Values.reports }}
+			{{- if (eq $c $.Values.reportId) }}
+			<option selected="selected" value="{{ $c }}">{{ $c }}</option>
+			{{- else }}
+			<option value="{{ $c }}">{{ $c }}</option>
+			{{- end }}
+		{{- end }}
+	</select>
+	<button onclick="onGenerate()">Generate</button>
+	
+	<p> Data: </p>
+	<div id="jsoneditor" style="width: 600px; height: 400px;"></div>
+	<p>Result:</p>
+	<div style="border: 2px solid; padding: 20px">
+		<img src="data:image/png;base64, {{ .Values.image }}" alt="Report" />
+	</div>
+
+	<script>
+		const container = document.getElementById("jsoneditor")
+		const options = {
+			mode: 'code',
+			modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+			onError: function (err) {
+			  alert(err.toString())
+			},
+			onModeChange: function (newMode, oldMode) {
+			  console.log('Mode switched from', oldMode, 'to', newMode)
+			}
+	  	}
+		const editor = new JSONEditor(container, options)
+		const initialJson = {{ .Values.data }}
+		editor.set(initialJson)
+
+		function onGenerate() {
+			const json = editor.get()
+			const objJsonStr = JSON.stringify(json);
+			const objJsonB64 = btoa(objJsonStr);
+
+			var e = document.getElementById("reports");
+			var reportId = e.value;
+
+			window.location.href = "http://localhost:8080/reports/preview/" + reportId + "?d=" + objJsonB64;
+		}
+	</script>
 </div>
 
 </body>
--- cmd/http_server.go
+++ cmd/http_server.go
@@ -24,7 +24,7 @@ func (s *Server) setupServer() *gin.Engine {
 	r.GET("/health", handlers.Health())
 	r.GET("/reports/list", handlers.ReportListHandler(s.templateService))
 	r.GET("/reports/render/:reportId", handlers.ReportRenderHandlder(s.templateService))
-	r.GET("/reports/preview/:reportId", handlers.ReportPreviewHandler(s.templateEngine, s.reportService))
+	r.GET("/reports/preview/:reportId", handlers.ReportPreviewHandler(s.templateService, s.templateEngine, s.reportService))
 	r.POST("/reports/export/html/:reportId", handlers.ReportExportHandler(s.reportService, "html"))
 	r.POST("/reports/export/png/:reportId", handlers.ReportExportHandler(s.reportService, "png"))
 	r.POST("/reports/export/pdf/:reportId", handlers.ReportExportHandler(s.reportService, "pdf"))

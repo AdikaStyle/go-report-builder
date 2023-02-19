a6870f530aa6a990a40a3771d270d1ad97ee6978
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-02-19 11:50:44 +0200

    feat: BulkAPI for fiber middleware

--- http/fiber/handlers/report_export_handler.go
+++ http/fiber/handlers/report_export_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -40,10 +41,81 @@ func ReportExportHandler(reportService service.ReportService, kind string) func(
 				})
 		}
 
-		return ctx.JSON(fiber.Map{
-			"reportId": reportId,
-			"data":     string(export),
-			"type":     kind,
+		return ctx.JSON(ExportResponse{
+			ID:   reportId,
+			Data: string(export),
+			Type: kind,
 		})
+	}
+}
+
+type ExportResponse struct {
+	ID   string `json:"reportId"`
+	Data string `json:"data"`
+	Type string `json:"type"`
+}
+
+type BulkExportEntry struct {
+	ID   string `json:"_id"`
+	Data any    `json:"data"`
+}
+
+type BulkExportRequest struct {
+	ID      string            `json:"_id"`
+	Entries []BulkExportEntry `json:"entries"`
+}
+
+type BulkExportResponse struct {
+	ID       string           `json:"_id"`
+	ReportID string           `json:"reportId"`
+	Reports  []ExportResponse `json:"reports"`
+}
+
+func BulkReportExportHandler(reportService service.ReportService, kind string) func(*fiber.Ctx) error {
+	return func(ctx *fiber.Ctx) error {
+		reportId := strings.TrimPrefix(ctx.Params("*"), "/")
+		body := new(BulkExportRequest)
+		if err := ctx.BodyParser(&body); err != nil {
+			logrus.Error(err)
+			return ctx.Status(http.StatusInternalServerError).
+				JSON(fiber.Map{
+					"err": err.Error(),
+				})
+		}
+
+		bulkResponse := &BulkExportResponse{
+			ID:       body.ID,
+			ReportID: reportId,
+			Reports:  make([]ExportResponse, 0),
+		}
+
+		for _, entry := range body.Entries {
+			entryReal := entry
+			reportData := entryReal.Data
+			var export []byte
+			var err error
+			switch kind {
+			case "html":
+				export, err = reportService.ExportReportHtml(reportId, reportData)
+			case "pdf":
+				export, err = reportService.ExportReportPdf(reportId, reportData)
+			case "png":
+				export, err = reportService.ExportReportPng(reportId, reportData)
+			}
+			if err != nil {
+				logrus.Error(err)
+				return ctx.Status(http.StatusInternalServerError).
+					JSON(fiber.Map{
+						"err": err.Error(),
+					})
+			}
+			bulkResponse.Reports = append(bulkResponse.Reports, ExportResponse{
+				ID:   fmt.Sprintf("%s:%s", reportId, entryReal.ID),
+				Data: string(export),
+				Type: kind,
+			})
+		}
+
+		return ctx.JSON(bulkResponse)
 	}
 }
--- http/fiber/middleware.go
+++ http/fiber/middleware.go
@@ -10,7 +10,12 @@ func Use(app fiber.Router, s *greypot.Module) {
 	app.Get("/reports/list", handlers.ReportListHandler(s.TemplateService))
 	app.Get("/reports/preview/*", handlers.ReportPreviewHandler(s.TemplateService, s.TemplateEngine, s.ReportService))
 	app.Get("/reports/render/*", handlers.ReportRenderHandlder(s.TemplateService))
+
 	app.Post("/reports/export/html/*", handlers.ReportExportHandler(s.ReportService, "html"))
 	app.Post("/reports/export/png/*", handlers.ReportExportHandler(s.ReportService, "png"))
 	app.Post("/reports/export/pdf/*", handlers.ReportExportHandler(s.ReportService, "pdf"))
+
+	app.Post("/reports/export/bulk/html/*", handlers.BulkReportExportHandler(s.ReportService, "html"))
+	app.Post("/reports/export/bulk/png/*", handlers.BulkReportExportHandler(s.ReportService, "png"))
+	app.Post("/reports/export/bulk/pdf/*", handlers.BulkReportExportHandler(s.ReportService, "pdf"))
 }
